60d8df2a3d04a3dec1e84f03aa21c1aadf81aed2
Zach_Kobrinsky <zachkobrinsky@gmail.com>
2020-09-11 13:47:42 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -37,14 +37,14 @@ class ApplicationController < Sinatra::Base
   end
 
   patch '/articles/:id' do
-    # binding.pry
     article = Article.find(params[:id])
     article.update(title: params[:title], content: params[:content])
     redirect to "/articles/#{article.id}"
   end
 
   delete '/articles/:id' do
-
+    Article.find(params[:id]).destroy
+    redirect to "/articles"
   end
 
   
